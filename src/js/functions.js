b2f6b2f8ce9c05d959491ea1549ad9b5593b3aa0
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@s4b.team>
2018-03-11 20:25:04 +0200

    Initial commit

---
+++ src/js/functions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var $ = require('jquery');
+
+module.exports = {
+	/*
+	 * Functions to be exported go here. Format:
+	 * exportName: functionName
+	 */
+};
+
+// Code goes here
---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var $ = window.$ = window.jQuery = require('jquery');
+var functions = require('./functions');
+
+/*
+ * Exported in 'functions.js' functions can be used here:
+ * functions.exportName()
+ */
+
+$(function () {
+
+	// Code goes here
+	console.log($(document).height());
+	$('.dotter-section').css('height', $(document).height() + 'px');
+	
+	
+	var $mobileMenu = $('.mobile-menu-wrapper');
+	
+	$('[data-mobile]').click(function(){
+		$mobileMenu.addClass('active');
+	});
+	
+	$mobileMenu.click(function(e){
+		if($(e.target).closest('.mobile-menu-wrapper .mobile-menu-box').length == 0) {
+			$mobileMenu.removeClass('active');
+		}
+	});
+});
